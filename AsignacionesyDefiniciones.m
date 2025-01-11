f8897574c431422b4235ffc56cde5b8558949a9b
tecnarf <francgomez.tec@gmail.com>
2025-01-10 23:17:21 -0300

    Add files via upload

---
+++ AsignacionesyDefiniciones.m
@@ -0,0 +1,8 @@
+clear; %limpia todas las variables del workspace
+close all; %cierra todas las figuras
+clc; %vacia el command window
+
+%% Asignaciones y definiciones
+var_a = 3;
+var_b = var_a;%podemos asignar el valor de otra variable a una nueva
+var_c = var_a*var_b;
