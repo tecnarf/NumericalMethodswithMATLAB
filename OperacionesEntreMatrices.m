4a8687badb340ecd89f9d28f4ec018ba9e3f31db
tecnarf <francgomez.tec@gmail.com>
2025-01-13 10:36:15 -0300

    Add files via upload

---
+++ OperacionesEntreMatrices.m
@@ -0,0 +1,20 @@
+clear; %limpia todas las variables del workspace
+close all; %cierra todas las figuras
+clc; %vacia el command window
+
+%% Operaciones entre matrices
+
+Matriz1 = [1 -2 1; 2 -1 4; 3 -2 2]; %Una nueva fila se escribe despues del punto y coma
+Matriz1Transpuesta = Matriz1'; %comilla simple ' con alt+39
+Matriz2 = Matriz1Transpuesta;
+
+%Suma y resta de matrices
+Matriz_suma = Matriz1 + Matriz2;
+Matriz_resta = Matriz2 - Matriz1;
+%Producto de matrices(recordar concidicion necesaria)
+Matriz_producto = Matriz1*Matriz2;
+%Division de matrices(Por la derecha y por la izquierda)
+%Por la derecha(Multiplica una matriz por la inversa de la otra por la derecha)
+div_derecha = Matriz1/Matriz2;%Multiplica la Matriz1 por la inversa de la Matriz2 por la derecha 
+%Por la izquierda(Multiplica una matriz por la inversa de la otra por la izquierda)
+div_izq = Matriz1\Matriz2;%Multiplica la inversa de la Matriz1 por la izquierda de la Matriz2%% Operaciones entre matrices
