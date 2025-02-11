efcbfafd3df100fe5e3b1e4c0726d22e07b6060b
tecnarf <francgomez.tec@gmail.com>
2025-02-11 07:37:04 -0300

    Delete AsignacionesyDefiniciones.m

--- AsignacionesyDefiniciones.m
+++
@@ -1,8 +0,0 @@
-clear; %limpia todas las variables del workspace
-close all; %cierra todas las figuras
-clc; %vacia el command window
-
-%% Asignaciones y definiciones
-var_a = 3;
-var_b = var_a;%podemos asignar el valor de otra variable a una nueva
-var_c = var_a*var_b;
